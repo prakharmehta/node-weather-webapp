08b31a4ecd6f4b1cf2e13793df6dba9f3f5540a9
prakharnmehta <prakh.mehta@gmail.com>
2020-04-19 11:01:39 +0530

    Changed pic on about

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help me',
+        title: 'Help',
         name: 'Prakhar'
     })
 })
